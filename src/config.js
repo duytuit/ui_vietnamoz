59f85d0a39e7daf4e1a5e634f96fa8401fedc826
Duy Tu <duytu89@gmail.com>
2023-06-17 16:08:58 +0700

    dev

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports.SITE = {
+    name: 'TailNext',
+  
+    origin: 'https://tailnext.vercel.app',
+    basePathname: '/',
+    trailingSlash: false,
+  
+    title: 'Visa - VietNamOZ',
+    description: 'TailNext is a free and ready to start template to make your website using Next.js and Tailwind CSS.',
+  };
+  
