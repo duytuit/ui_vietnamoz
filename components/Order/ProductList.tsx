a4cb4857e3c13d0c5cb8dc5e87b7d0c51e70fe0b
Duy Tu <duytu89@gmail.com>
2023-07-03 11:38:04 +0700

    dev

--- components/Order/ProductList.tsx
+++ components/Order/ProductList.tsx
@@ -13,16 +13,12 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
       <h1 className="bp-h1 bp-h1-grey flex justify-center">
         No hidden fees! All taxes included.
       </h1>
-      <div className="search-results-control ng-star-inserted dark:bg-[#1D2144] xss:flex-col lg:flex-row">
+      <div className="lg:!flex-row search-results-control ng-star-inserted dark:bg-[#1D2144] ">
         <div className="visa-type-switcher flex xss:w-full lg:flex lg:w-auto lg:items-center lg:justify-center">
-          <div>
             <input id="single-entry" name="radio-category" type="radio" />
             <label htmlFor="single-entry">Single Entry</label>
-          </div>
-          <div>
             <input id="multiple-entry" name="radio-category" type="radio" />
             <label htmlFor="multiple-entry">Multiple Entry</label>
-          </div>
         </div>
         <div className="currency-switcher-wr ng-star-inserted xss:w-full lg:flex lg:w-auto lg:items-center lg:justify-center">
           <label className="currency-default" htmlFor="currency-switcher">
