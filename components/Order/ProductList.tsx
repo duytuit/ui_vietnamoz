71c24582ee0a7974c0d9ad248c4101c0c6a0ce26
Duy Tu <duytu89@gmail.com>
2023-07-03 11:18:34 +0700

    dev

--- components/Order/ProductList.tsx
+++ components/Order/ProductList.tsx
@@ -13,8 +13,8 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
       <h1 className="bp-h1 bp-h1-grey flex justify-center">
         No hidden fees! All taxes included.
       </h1>
-      <div className="search-results-control ng-star-inserted dark:bg-[#1D2144] xss:flex-col lg:flex-row">
-        <div className="visa-type-switcher flex xss:w-full lg:flex lg:w-auto lg:items-center lg:justify-center">
+      <div className="search-results-control ng-star-inserted dark:bg-[#1D2144] xss:!flex-col lg:!flex-row">
+        <div className="visa-type-switcher flex xss:!w-full lg:!flex lg:!w-auto lg:items-center lg:justify-center">
           <div>
             <input id="single-entry" name="radio-category" type="radio" />
             <label htmlFor="single-entry">Single Entry</label>
@@ -24,7 +24,7 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
             <label htmlFor="multiple-entry">Multiple Entry</label>
           </div>
         </div>
-        <div className="currency-switcher-wr ng-star-inserted xss:w-full lg:flex lg:w-auto lg:items-center lg:justify-center">
+        <div className="currency-switcher-wr ng-star-inserted xss:!w-full lg:!flex lg:!w-auto lg:items-center lg:justify-center">
           <label className="currency-default" htmlFor="currency-switcher">
             USD
           </label>
@@ -36,7 +36,7 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
         </div>
       </div>
       <div className="visa-packages-wr">
-        <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:w-full lg:w-[28%]">
+        <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:!w-full lg:!w-[28%]">
           <h1 className="product-name text-black dark:text-white">
             48 hours transit visa
           </h1>
@@ -57,7 +57,7 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
             proceed
           </Link>
         </div>
-        <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:w-full lg:w-[28%]">
+        <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:!w-full lg:!w-[28%]">
           <h1 className="product-name text-black dark:text-white">
             48 hours transit visa
           </h1>
@@ -78,7 +78,7 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
             proceed
           </Link>
         </div>
-        <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:w-full lg:w-[28%]">
+        <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:!w-full lg:!w-[28%]">
           <h1 className="product-name text-black dark:text-white">
             48 hours transit visa
           </h1>
