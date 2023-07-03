667c1c16e7927a0ab904d3a1e1bf7862e8192d80
Duy Tu <duytu89@gmail.com>
2023-07-03 09:17:26 +0700

    dev

--- components/Order/ProductList.tsx
+++ components/Order/ProductList.tsx
@@ -13,14 +13,18 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
       <h1 className="bp-h1 bp-h1-grey flex justify-center">
         No hidden fees! All taxes included.
       </h1>
-      <div className="search-results-control ng-star-inserted dark:bg-[#1D2144]">
-        <div className="visa-type-switcher flex">
-          <input id="single-entry" name="radio-category" type="radio" />
-          <label htmlFor="single-entry">Single Entry</label>
-          <input id="multiple-entry" name="radio-category" type="radio" />
-          <label htmlFor="multiple-entry">Multiple Entry</label>
+      <div className="search-results-control ng-star-inserted dark:bg-[#1D2144] xss:flex-col lg:flex-row">
+        <div className="visa-type-switcher flex xss:w-full lg:w-auto lg:flex lg:justify-center lg:items-center">
+          <div>
+            <input id="single-entry" name="radio-category" type="radio" />
+            <label htmlFor="single-entry">Single Entry</label>
+          </div>
+           <div>
+           <input id="multiple-entry" name="radio-category" type="radio" />
+            <label htmlFor="multiple-entry">Multiple Entry</label>
+           </div>
         </div>
-        <div className="currency-switcher-wr ng-star-inserted">
+        <div className="currency-switcher-wr ng-star-inserted xss:w-full lg:w-auto lg:flex lg:justify-center lg:items-center">
           <label className="currency-default" htmlFor="currency-switcher">
             USD
           </label>
@@ -32,7 +36,7 @@ const ProductList: FC<countryProps> = ({ country = null }) => {
         </div>
       </div>
       <div className="visa-packages-wr">
-      <div className="card-wr ng-star-inserted dark:bg-[#1D2144]">
+      <div className="card-wr ng-star-inserted dark:bg-[#1D2144] xss:w-full lg:w-[28%]">
             <h1 className="product-name dark:text-white text-black">48 hours transit visa</h1>
             <h1 className="product-type">Single Entry Visa</h1>
             <div className="card-line"></div>
