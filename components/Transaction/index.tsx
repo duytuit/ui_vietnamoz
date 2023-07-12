1f79b7648d87be02a41b43803cb039846da72451
Duy Tu <duytu89@gmail.com>
2023-07-12 18:59:29 +0700

    dev

--- components/Transaction/index.tsx
+++ components/Transaction/index.tsx
@@ -4,21 +4,113 @@ import { useState } from "react";
 
 const TransactionResult = (props) => {
   const param = useSearchParams();
-  console.log('ket_qua',param.toString());
+  console.log("ket_qua", param.toString());
   return (
-   <>
-     <section className="relative pt-16 md:pt-20 lg:pt-28 mb-[120px]">
-      <div className="container">
-        <div className="mb-8 w-full md:mb-12 lg:mb-16 mt-20">
-          <h1 className="bp-h1 black dark:text-white">Track Your Application</h1>
-          <h1 className="bp-h1 bp-h1-grey flex justify-center">
-          Applications made on weekends (Friday & Saturday) and public holidays may take longer to process.
-          </h1>
+    <>
+      <section className="relative mb-[120px] pt-16 md:pt-20 lg:pt-28">
+        <div className="container">
+          <div className="mb-8 mt-20 w-full md:mb-12 lg:mb-16">
+            <h1 className="bp-h1 black dark:text-white">Payment Transaction</h1>
+          </div>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <span>Giao dịch nhận thanh toán</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span>
+                    Dear, <span className="uppercase">Nguyễn Duy Tu</span>
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span>
+                  Bạn đã thực hiện giao dịch thanh toán thành công, vui lòng xem chi tiết giao dịch dưới đây.
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td align="left">
+                          <span>
+                            Khách hàng:
+                            <br />
+                            <span className="uppercase">Nguyễn Duy Tú</span>
+                            <br />
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            {/* Email: <a href="mailto:tund@dxmb.vn" target="_blank">duytu89@gmail.com</a><br/> */}
+                            Số điện thoại: 0366961008
+                            <br />
+                            Địa chỉ: Hà nội
+                            <br />
+                          </span>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="left">Nội dung thanh toán</td>
+                        <td>E Visa</td>
+                      </tr>
+                      <tr>
+                        <td align="left">Mã giao dịch</td>
+                        <td>VNE43543564355</td>
+                      </tr>
+                      <tr>
+                        <td align="left">Mã đơn hàng</td>
+                        <td>VNE43543564355</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="left">Giá trị đơn hàng</td>
+                        <td align="right">
+                          <strong>1000000</strong> VND
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="left">Số tiền thanh toán</td>
+                        <td align="right">
+                          <strong>10000000</strong> VND
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="left">Thời gian</td>
+                        <td>12/12/2023</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="left">Trạng thái giao dịch</td>
+                        <td>Thành công</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+              </tr>
+              <tr>
+                <td>
+                  <div>
+                    <a href="/">Nhấn vào đây để trở về trang chủ</a>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div>  Thanh toán thành công.</div>
-      </div>
-    </section>
-   </>
+      </section>
+    </>
   );
 };
 
