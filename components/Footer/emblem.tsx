361f64236f9bd94141f97ab4ed289035bc41fd05
Duy Tu <duytu89@gmail.com>
2023-07-29 21:55:09 +0700

    dev

--- components/Footer/emblem.tsx
+++ components/Footer/emblem.tsx
@@ -1,19 +1,22 @@
 "use client"
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import moment from "moment";
 
 const Emblem = () => {
+ 
   const _scrollTop =()=>{
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+  
   }
   return (
     <>
      <div className="ng-tns-c4-5 pt-16 md:pt-20 lg:pt-24">
           <div className="VietNam-time dark:text-black">
-            <div className="ml-9">VietNam Local Time - 1:18 PM</div>
+            <div className="ml-9">VietNam Local Time - {  moment(new Date()).format('HH:mm')}</div>
           </div>
           <div className="cta-wr dark:bg-black">
             <div className="bp-container">
@@ -21,7 +24,7 @@ const Emblem = () => {
               <div className="h-full p-4 xss:w-full lg:w-1/2 lg:flex lg:justify-center lg:items-center">
                 <div className="cta-column">
                     <h1 className="black dark:text-white text-[22px] font-[400]">
-                    Want your VN Visa by Wednesday, 28 Jun 2023?
+                    Want your VN Visa by {new Date().toDateString()}?
                     </h1>
                     <h1 className="bp-h1 pink">Apply within 5 Hrs 41 Mins</h1>
                   </div>
